a86ca38ac3c1760bb854ed1e624224b862b32487
Pawel Krupa <pawel.krupa.junior@gmail.com>
2018-09-06 10:19:50 +0200

    Initial commit

---
+++ src/main/java/com/codecool/pionierzy/gotchiarena/GotchiarenaApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.pionierzy.gotchiarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchiarenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotchiarenaApplication.class, args);
+	}
+}
