787976d4d29e2c14f62abfc8c1c24b48a874796b
Pico143 <pico19942@gmail.com>
2018-09-18 14:52:52 +0200

    bound registration to form

--- src/main/java/com/codecool/pionierzy/gotchiarena/web/MainController.java
+++ src/main/java/com/codecool/pionierzy/gotchiarena/web/MainController.java
@@ -1,8 +1,11 @@
 package com.codecool.pionierzy.gotchiarena.web;
 
+import com.codecool.pionierzy.gotchiarena.model.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import java.security.Principal;
 
@@ -15,7 +18,9 @@ public class MainController {
     }
 
     @RequestMapping("/login")
-    public String login(Principal principal) {
+    public String login(Principal principal, WebRequest request, Model model) {
+        UserDTO userDTO = new UserDTO();
+        model.addAttribute("user", userDTO);
         return principal != null ? "redirect:/lobby" : "login";
     }
 
