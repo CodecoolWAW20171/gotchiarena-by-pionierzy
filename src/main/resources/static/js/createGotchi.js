5e16579c0ded6fd5cd76cd2397a7c7cd84c4d689
Witold Tafel <witold.tafel@gmail.com>
2018-09-20 00:29:30 +0200

    gotchi form constraints

--- src/main/resources/static/js/createGotchi.js
+++ src/main/resources/static/js/createGotchi.js
@@ -1,18 +1,51 @@
-document.getElementById("speed").addEventListener("input",updatePointsLeft);
-document.getElementById("attack").addEventListener("input",updatePointsLeft);
-document.getElementById("defence").addEventListener("input",updatePointsLeft);
+var speed = document.getElementById("speed");
+var attack = document.getElementById("attack");
+var defence = document.getElementById("defence");
+
+speed.addEventListener("input", updatePointsLeft);
+attack.addEventListener("input", updatePointsLeft);
+defence.addEventListener("input", updatePointsLeft);
 
 
-
-function updatePointsLeft(){
-    let speedValue = document.getElementById("speed").value;
-    let attackValue = document.getElementById("attack").value;
-    let defenceValue = document.getElementById("defence").value;
+function updatePointsLeft() {
+    let speedValue = Number(speed.value);
+    let attackValue = Number(attack.value);
+    let defenceValue = Number(defence.value);
 
     let pointsLeft = 200 - speedValue - attackValue - defenceValue;
-    document.getElementById("speed").setAttribute("max",pointsLeft + speedValue);
-    document.getElementById("attack").setAttribute("max",pointsLeft + attackValue);
-    document.getElementById("defence").setAttribute("max",pointsLeft + defenceValue);
-    document.getElementById("pointsLeft").innerText  = pointsLeft >= 0? pointsLeft.toString() : 0;
-
+    speed.setAttribute("max", pointsLeft + speedValue);
+    attack.setAttribute("max", pointsLeft + attackValue);
+    defence.setAttribute("max", pointsLeft + defenceValue);
+    document.getElementById("pointsLeft").innerText = pointsLeft > 0 ? pointsLeft : "no";
 }
+
+document.getElementById("type").addEventListener("change", setSecondarySelect);
+
+function setSecondarySelect(event) {
+    var coolTypes = Array.from(document.getElementsByClassName("cool"));
+    var lameTypes = Array.from(document.getElementsByClassName("lame"));
+
+
+    if (["WATER","FIRE","ICE"].includes(event.target.value)) {
+        coolTypes.forEach(e => {
+            e.disabled = true;
+        });
+        lameTypes.forEach(e => {
+            e.disabled = false;
+        });
+
+    } else {
+        coolTypes.forEach(e => {
+            e.disabled = false;
+        });
+        lameTypes.forEach(e => {
+            e.disabled = true;
+        });
+    }
+}
+
+function lockTypeSelect() {
+    document.getElementById("type").disabled = true;
+}
+
+document.getElementById("secondary").addEventListener("change", lockTypeSelect);
