0b155c2ec622081172a84ef9d17d5b4cb0bdf9fe
Witold Tafel <witold.tafel@gmail.com>
2018-09-20 00:44:57 +0200

    locked selects

--- src/main/resources/static/js/createGotchi.js
+++ src/main/resources/static/js/createGotchi.js
@@ -1,6 +1,6 @@
-var speed = document.getElementById("speed");
-var attack = document.getElementById("attack");
-var defence = document.getElementById("defence");
+let speed = document.getElementById("speed");
+let attack = document.getElementById("attack");
+let defence = document.getElementById("defence");
 
 speed.addEventListener("input", updatePointsLeft);
 attack.addEventListener("input", updatePointsLeft);
@@ -22,27 +22,31 @@ function updatePointsLeft() {
 document.getElementById("type").addEventListener("change", setSecondarySelect);
 
 function setSecondarySelect(event) {
-    var coolTypes = Array.from(document.getElementsByClassName("cool"));
-    var lameTypes = Array.from(document.getElementsByClassName("lame"));
+    let wetFireTypes = Array.from(document.getElementsByClassName("cool"));
+    let bushyLightningTypes = Array.from(document.getElementsByClassName("lame"));
+    document.getElementById("secondary").hidden = false;
 
 
     if (["WATER","FIRE","ICE"].includes(event.target.value)) {
-        coolTypes.forEach(e => {
+        wetFireTypes.forEach(e => {
             e.disabled = true;
         });
-        lameTypes.forEach(e => {
+        bushyLightningTypes.forEach(e => {
             e.disabled = false;
         });
 
     } else {
-        coolTypes.forEach(e => {
+        wetFireTypes.forEach(e => {
             e.disabled = false;
         });
-        lameTypes.forEach(e => {
+        bushyLightningTypes.forEach(e => {
             e.disabled = true;
         });
     }
 }
+
+
+
 
 function lockTypeSelect() {
     document.getElementById("type").disabled = true;
