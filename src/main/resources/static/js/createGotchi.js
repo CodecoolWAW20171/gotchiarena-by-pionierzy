c0b2964c204e6ad0993cec721c80f4ad17a61fb4
Witold Tafel <witold.tafel@gmail.com>
2018-09-19 15:15:40 +0200

    js to gotchiForm

--- src/main/resources/static/js/createGotchi.js
+++ src/main/resources/static/js/createGotchi.js
@@ -1,3 +1,18 @@
+document.getElementById("speed").addEventListener("input",updatePointsLeft);
+document.getElementById("attack").addEventListener("input",updatePointsLeft);
+document.getElementById("defence").addEventListener("input",updatePointsLeft);
 
 
 
+function updatePointsLeft(){
+    let speedValue = document.getElementById("speed").value;
+    let attackValue = document.getElementById("attack").value;
+    let defenceValue = document.getElementById("defence").value;
+
+    let pointsLeft = 200 - speedValue - attackValue - defenceValue;
+    document.getElementById("speed").setAttribute("max",pointsLeft + speedValue);
+    document.getElementById("attack").setAttribute("max",pointsLeft + attackValue);
+    document.getElementById("defence").setAttribute("max",pointsLeft + defenceValue);
+    document.getElementById("pointsLeft").innerText  = pointsLeft >= 0? pointsLeft.toString() : 0;
+
+}
