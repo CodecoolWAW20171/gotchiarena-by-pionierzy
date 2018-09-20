82d507b0628d0ed575d4a6fd39ad175c86816793
Pico143 <pico19942@gmail.com>
2018-09-20 10:30:11 +0200

    created a map of strong attacks and weak attacks

--- src/main/java/com/codecool/pionierzy/gotchiarena/service/FightServices/FightServiceImpl.java
+++ src/main/java/com/codecool/pionierzy/gotchiarena/service/FightServices/FightServiceImpl.java
@@ -1,13 +1,34 @@
 package com.codecool.pionierzy.gotchiarena.service.FightServices;
 
+import com.codecool.pionierzy.gotchiarena.model.AttackType;
 import com.codecool.pionierzy.gotchiarena.model.Gotchi;
 import com.codecool.pionierzy.gotchiarena.model.Room;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class FightServiceImpl implements FightService {
+    private Map<AttackType, AttackType> strongAgainst;
+    private Map<AttackType, AttackType> weakAgainst;
 
     private HashMap<Room, RoundMessage> roomMap;
+
+    public FightServiceImpl() {
+        strongAgainst = new HashMap<AttackType, AttackType>();
+        weakAgainst = new HashMap<AttackType, AttackType>();
+
+        strongAgainst.put(AttackType.LIGHTNING, AttackType.PLANT);
+        strongAgainst.put(AttackType.PLANT, AttackType.EARTH);
+        strongAgainst.put(AttackType.EARTH, AttackType.LIGHTNING);
+        strongAgainst.put(AttackType.FIRE, AttackType.ICE);
+        strongAgainst.put(AttackType.ICE, AttackType.WATER);
+        strongAgainst.put(AttackType.WATER, AttackType.FIRE);
+        for (Map.Entry<AttackType, AttackType> entry : strongAgainst.entrySet()) {
+            AttackType strong = entry.getKey();
+            AttackType weak = entry.getValue();
+            weakAgainst.put(weak, strong);
+        }
+    }
 
     @Override
     public void receiveAction(Gotchi gotchi) {
@@ -16,7 +37,6 @@ public class FightServiceImpl implements FightService {
 
     @Override
     public void resolveRound(Room room) {
-
         //after round we want to get a clear round
         this.roomMap.put(room, new RoundMessage());
     }
