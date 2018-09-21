8f6a2a7b60203855c394d281c84d279ce845f7a7
Witold Tafel <witold.tafel@gmail.com>
2018-09-21 02:37:16 +0200

    fixed js for gotchi form and generate selects dynamicly

--- src/main/java/com/codecool/pionierzy/gotchiarena/model/GotchiType.java
+++ src/main/java/com/codecool/pionierzy/gotchiarena/model/GotchiType.java
@@ -1,10 +1,20 @@
 package com.codecool.pionierzy.gotchiarena.model;
 
 public enum GotchiType {
-    FIRE,
-    WATER,
-    ICE,
-    LIGHTNING,
-    PLANT,
-    EARTH
+    FIRE("wetFire"),
+    WATER("wetFire"),
+    ICE("wetFire"),
+    LIGHTNING("bushyLightning"),
+    PLANT("bushyLightning"),
+    EARTH("bushyLightning");
+
+    private final String groupType;
+
+    GotchiType(String groupType) {
+        this.groupType = groupType;
+    }
+
+    public String getGroupType() {
+        return groupType;
+    }
 }
