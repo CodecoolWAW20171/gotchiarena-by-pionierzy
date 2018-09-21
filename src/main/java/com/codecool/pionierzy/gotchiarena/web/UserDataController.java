5eccd61c46c8a10a09a4c06acd06679d328f3d8b
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-09-22 00:04:36 +0200

    new colors

--- src/main/java/com/codecool/pionierzy/gotchiarena/web/UserDataController.java
+++ src/main/java/com/codecool/pionierzy/gotchiarena/web/UserDataController.java
@@ -1,4 +1,26 @@
 package com.codecool.pionierzy.gotchiarena.web;
 
+import com.codecool.pionierzy.gotchiarena.service.GotchiServices.GotchiService;
+import com.codecool.pionierzy.gotchiarena.service.UserServices.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserDataController {
+
+    private final UserService userService;
+    private final GotchiService gotchiService;
+
+    @Autowired
+    public UserDataController(UserService userService, GotchiService gotchiService) {
+        this.userService = userService;
+        this.gotchiService = gotchiService;
+    }
+
+//    @RequestMapping("/user/you")
+//    public String user(){
+//
+//    }
+
 }
